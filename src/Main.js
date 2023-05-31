e622316bed4ac3e5b110c4a6fbcf0a046e3c0060
Vinu Cyril <vinucyril@gmail.com>
2023-06-01 01:41:47 +0530

    desktopView

---
+++ src/Main.js
@@ -0,0 +1,28 @@
+import data from "./data.json";
+
+const Main = () => {
+  return (
+    <main>
+      {data.map((item) => (
+        <div key={item.id} className="card">
+          <div
+            style={{ backgroundColor: item.logoBackground }}
+            className="logo-container"
+          >
+            <img src={item.logo} alt={item.company}></img>
+          </div>
+
+          <div className="time">
+            <p>{item.postedAt}</p>
+            <p>{item.contract}</p>
+          </div>
+          <h1>{item.position}</h1>
+          <p>{item.company}</p>
+          <h2>{item.location}</h2>
+        </div>
+      ))}
+    </main>
+  );
+};
+
+export default Main;
